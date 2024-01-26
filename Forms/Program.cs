495573942284daa81872ce01f8d06f61d005007f
Nikita <Kalyazin.nik@outlook.com>
2024-01-26 22:53:16 +0300

    Добавьте файлы проекта.

---
+++ Forms/Program.cs
@@ -0,0 +1,12 @@
+namespace Forms
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run();
+        }
+    }
+}
