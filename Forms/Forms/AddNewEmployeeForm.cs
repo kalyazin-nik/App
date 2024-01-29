8a01dc0b9d6806c313ce5ff2d9983001aebf88df
Nikita <Kalyazin.nik@outlook.com>
2024-01-30 02:05:38 +0300

    Change AddNewEmployeeForm

--- Forms/Forms/AddNewEmployeeForm.cs
+++ Forms/Forms/AddNewEmployeeForm.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
+using System.Net.Mail;
 using Database;
 using Entities;
 
@@ -12,6 +13,7 @@ internal class AddNewEmployeeForm : Form
     private TextBox[] _textBoxes = null!;
     private DateTimePicker _birthDayBox = null!;
     private MaskedTextBox _phoneNumberBox = null!;
+    private TextBox _mailAddressBox = null!;
     private ComboBox _familyStatusBox = null!;
     private TextBox _textBoxAddress = null!;
     private TextBox _textBoxHobbies = null!;
@@ -30,6 +32,7 @@ internal class AddNewEmployeeForm : Form
         await MakeTextBoxesAsync();
         await MakeBirthDayBoxAsync();
         await MakePhoneNumberBoxAsync();
+        await MakeMailAddressBoxAsync();
         await MakeFamilyStatusBoxAsync();
         await MakeTextBoxAddressAsync();
         await MakeTextBoxHobbiesAsync();
@@ -43,7 +46,7 @@ internal class AddNewEmployeeForm : Form
         FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
-        //TopMost = true;
+        TopMost = true;
 
         await Task.CompletedTask;
     }
@@ -88,7 +91,6 @@ internal class AddNewEmployeeForm : Form
             new() { Location = new Point(250, 80), Size = textBoxesSize, TabIndex = 1, AutoSize = false },
             new() { Location = new Point(250, 130), Size = textBoxesSize, TabIndex = 2, AutoSize = false },
             new() { Location = new Point(250, 180), Size = textBoxesSize, TabIndex = 3, AutoSize = false },
-            new() { Location = new Point(250, 330), Size = textBoxesSize, TabIndex = 6, AutoSize = false },
             new() { Location = new Point(250, 430), Size = textBoxesSize, TabIndex = 8, AutoSize = false },
         ];
 
@@ -146,6 +148,26 @@ internal class AddNewEmployeeForm : Form
         };
 
         Controls.Add(_phoneNumberBox);
+        await Task.CompletedTask;
+    }
+
+    private async Task MakeMailAddressBoxAsync()
+    {
+        _mailAddressBox = new TextBox()
+        {
+            Location = new Point(250, 330),
+            Size = new Size(300, 30),
+            TabIndex = 6,
+            AutoSize = false
+        };
+        _mailAddressBox.KeyPress += new KeyPressEventHandler(CorrectKeyPress!);
+        _mailAddressBox.Enter += (sender, args) =>
+        {
+            if (sender is TextBox mailAddress)
+                RemoveErrorLabel(mailAddress.Location.X - 15, mailAddress.Location.Y);
+        };
+
+        Controls.Add(_mailAddressBox);
         await Task.CompletedTask;
     }
 
@@ -212,13 +234,13 @@ internal class AddNewEmployeeForm : Form
         };
 
         Controls.Add(_textBoxHobbies);
-        Controls.Add(new Label() 
+        Controls.Add(new Label()
         {
             Text = "Перечисления выполнять через запятую",
             Font = new Font("Tahoma", 8, FontStyle.Regular),
-            Location = new Point(250, 655), 
+            Location = new Point(250, 655),
             Size = new Size(350, 30),
-            ForeColor = Color.Gray 
+            ForeColor = Color.Gray
         });
         await Task.CompletedTask;
     }
@@ -244,7 +266,7 @@ internal class AddNewEmployeeForm : Form
 
         if (textBox is not null && keyPress is not null)
         {
-            if (keyPress.KeyChar == ' ') keyPress.KeyChar = '\0';
+            if (textBox.TabIndex != 0 && keyPress.KeyChar == ' ') keyPress.KeyChar = '\0';
             else if (textBox.Text.Length == 0)
             {
                 if ((keyPress.KeyChar >= 1072 && keyPress.KeyChar <= 1103) || (keyPress.KeyChar >= 97 && keyPress.KeyChar <= 122))
@@ -269,10 +291,19 @@ internal class AddNewEmployeeForm : Form
         if (_phoneNumberBox.Text.Length < 16)
             flag = TryMakeErrorLabel(_phoneNumberBox.Location.X - 15, _phoneNumberBox.Location.Y);
 
+        try
+        {
+            _ = new MailAddress(_mailAddressBox.Text);
+        }
+        catch (Exception)
+        {
+            flag = TryMakeErrorLabel(_mailAddressBox.Location.X - 15, _mailAddressBox.Location.Y);
+        }
+
         if (_familyStatusBox.SelectedIndex == -1)
             flag = TryMakeErrorLabel(_familyStatusBox.Location.X - 15, _familyStatusBox.Location.Y);
 
-        if(_textBoxAddress.Text == "")
+        if (_textBoxAddress.Text == "")
             flag = TryMakeErrorLabel(_textBoxAddress.Location.X - 15, _textBoxAddress.Location.Y);
 
         return flag;
@@ -292,15 +323,14 @@ internal class AddNewEmployeeForm : Form
 
     private void RemoveErrorLabel(int x, int y)
     {
+        var point = new Point(x, y);
+
         for (int i = 0; i < Controls.Count; i++)
-        {
-            var point = new Point(x, y);
             if (Controls[i].Location == point)
             {
                 Controls.RemoveAt(i);
                 break;
             }
-        }
     }
 
     private void CreateEmployee(object sender, EventArgs args)
@@ -314,12 +344,12 @@ internal class AddNewEmployeeForm : Form
                 _textBoxes[3].Text,
                 _birthDayBox.Value,
                 _phoneNumberBox.Text,
-                _textBoxes[4].Text,
+                _mailAddressBox.Text,
                 _familyStatusBox.Text,
-                _textBoxes[5].Text,
+                _textBoxes[4].Text,
                 _textBoxAddress.Text,
                 _textBoxHobbies.Text is "" ? null :
-                    _textBoxHobbies.Text.Split(',').Select(s => s.Trim()).Where(s => s.Length > 0).ToList(),
+                    _textBoxHobbies.Text.Split(',').Select(s => s.Trim().ToLower()).Where(s => s.Length > 0).ToList(),
                 DateTime.Now);
             _dataContext.Employees.Add(employee);
             _dataContext.SaveChanges();
