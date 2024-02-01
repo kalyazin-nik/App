87d1a3ae583c213f3bdd685df1736a7637ef1410
Nikita <Kalyazin.nik@outlook.com>
2024-02-01 11:20:36 +0300

    Reapply "Complete"
    This reverts commit a0dc18f9158574540b978c6f90c6fe7627fac246.

--- Entityes/Employee.cs
+++ Entityes/Employee.cs
@@ -1,19 +1,37 @@
 ﻿namespace Entities;
 
-public sealed class Employee(string post, string surname, string name, string patronymic,
-    DateTime dateOfBirth, string phoneNumber, string mail, string familyStatus, string city,
-    string address, List<string>? hobbies, DateTime createdAt) : BaseEntity(createdAt)
+public sealed class Employee : BaseEntity
 {
     public int Id { get; set; }
-    public string Post { get; set; } = post;
-    public string Surname { get; set; } = surname;
-    public string Name { get; set; } = name;
-    public string Patronymic { get; set; } = patronymic;
-    public DateTime DateOfBirth { get; set; } = dateOfBirth;
-    public string PhoneNumber { get; set; } = phoneNumber;
-    public string Mail { get; set; } = mail;
-    public string FamilyStatus { get; set; } = familyStatus;
-    public string City { get; set; } = city;
-    public string Address { get; set; } = address;
-    public List<string>? Hobbies { get; set; } = hobbies;
+    public string Post { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Patronymic { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; } = default!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Mail { get; set; } = null!;
+    public string FamilyStatus { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public List<string>? Hobbies { get; set; } = null!;
+
+    public Employee(string post, string surname, string name, string patronymic,
+    DateTime dateOfBirth, string phoneNumber, string mail, string familyStatus, string city,
+    string address, List<string>? hobbies, DateTime createdAt) : base(createdAt)
+    {
+        Post = post;
+        Surname = surname;
+        Name = name;
+        Patronymic = patronymic;
+        DateOfBirth = dateOfBirth;
+        PhoneNumber = phoneNumber;
+        Mail = mail;
+        FamilyStatus = familyStatus;
+        City = city;
+        Address = address;
+        Hobbies = hobbies;
+        CreatedAt = createdAt;
+    }
+
+    public Employee() : base(DateTime.Now) { }
 }
